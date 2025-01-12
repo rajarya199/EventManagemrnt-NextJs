d65510b03d1a32acf5f5548d62b63d7a437fd17f
rajarya199 <aryalrajan078@gmail.com>
2025-01-12 12:18:05 +0545

    utility,type

---
+++ app/lib/utiils.ts
@@ -0,0 +1,6 @@
+import qs from "query-string"
+export const handleError = (error: unknown) => {
+    console.error(error)
+    throw new Error(typeof error === 'string' ? error : JSON.stringify(error))
+ 
+}
---
+++ types/index.ts
@@ -0,0 +1,8 @@
+// ====== USER PARAMS
+export type CreateUserParams = {
+    clerkId: string
+    fname: string
+    lname: string
+    email: string
+    image: string
+  }
