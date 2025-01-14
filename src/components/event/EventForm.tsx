ac6a1bdbe960a8928ae52ca454f74422c68194ea
rajarya199 <aryalrajan078@gmail.com>
2025-01-14 23:00:37 +0545

    event form-i

--- src/components/event/EventForm.tsx
+++ src/components/event/EventForm.tsx
@@ -14,39 +14,37 @@ import {
   FormMessage,
 } from "@/src/components/ui/form"
 import { Input } from '../ui/input'
+import { eventInitialValues } from '@/src/constants'
+import { eventFormSchema } from '@/src/lib/schema'
+import Dropdown from '../usable/Dropdown'
 
 type EventFormProps={
     userId:string
     eventType:"Add" | "update"
 }
-const formSchema = z.object({
-    username: z.string().min(2, {
-      message: "Username must be at least 2 characters.",
-    }),
-  })
+
 
 const EventForm = ({userId,eventType}:EventFormProps) => {
-     const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
+     const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues: eventInitialValues,
   })
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof eventFormSchema>) {
     console.log(values)
   }
   return (
-    <div>EventForm {eventType}
+    <div className=''>EventForm {eventType}
      <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 flex flex-col gap-5">
+        <div className='flex flex-col gap-5 mt-4 md:flex-row'>
         <FormField
           control={form.control}
-          name="username"
+          name="title"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
+            <FormItem className='w-full'>
+              <FormLabel className='block text-sm font-semibold text-gray-800'>Event Title</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input className='input-field' placeholder="event title" {...field} />
               </FormControl>
               <FormDescription>
                 This is your public display name.
@@ -55,6 +53,28 @@ const EventForm = ({userId,eventType}:EventFormProps) => {
             </FormItem>
           )}
         />
+                <FormField
+          control={form.control}
+          name="eventDescription"
+          render={({ field }) => (
+            <FormItem className='w-full'>
+              <FormLabel>Event Category</FormLabel>
+              <FormControl>
+                <Dropdown onChangeHandler={field.onChange} value={field.value}/>
+              </FormControl>
+              <FormDescription>
+                Select category
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+       
+        
+        </div>
+        <div className='felx flex-col gap-5 md:flex-row'>
+
+        </div>
         <Button type="submit">Submit</Button>
       </form>
     </Form>
