5aed996d6ca8be60deb08b45d3e80fcb3d2027c4
rajarya199 <aryalrajan078@gmail.com>
2025-01-15 21:59:21 +0545

    event for-ii

--- src/components/event/EventForm.tsx
+++ src/components/event/EventForm.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React from 'react'
+import Image from 'next/image'
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
@@ -14,6 +15,18 @@ import {
   FormMessage,
 } from "@/src/components/ui/form"
 import { Input } from '../ui/input'
+import { Textarea } from "@/src/components/ui/textarea"
+ 
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/src/components/ui/select"
+
 import { eventInitialValues } from '@/src/constants'
 import { eventFormSchema } from '@/src/lib/schema'
 import Dropdown from '../usable/Dropdown'
@@ -72,8 +85,92 @@ const EventForm = ({userId,eventType}:EventFormProps) => {
        
         
         </div>
-        <div className='felx flex-col gap-5 md:flex-row'>
+        <div className='flex flex-col gap-5 md:flex-row'>
+        <FormField
+              control={form.control}
+              name="eventDescription"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                   <FormLabel className='block text-sm font-semibold text-gray-800'>Event description</FormLabel>
 
+                  <FormControl className="h-72">
+                    <Textarea placeholder="Description" {...field} className="textarea rounded-2xl" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+             <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className='w-full'>
+              <FormLabel className='block text-sm font-semibold text-gray-800'>Event Title</FormLabel>
+              <FormControl className='h-72'>
+                <Input className='input-field' placeholder="event title" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        </div>
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+              control={form.control}
+              name="location"
+              render={({ field }) => (
+                <FormItem className="w-full">
+               <FormLabel className='block text-sm font-semibold text-gray-800'>Location</FormLabel>
+
+                  <FormControl>
+                    <div className="flex-center h-[54px] w-full overflow-hidden rounded-2xl bg-grey-50 px-4 py-2">
+                      <Image
+                        src="/assets/icons/location-grey.svg"
+                        alt="location"
+                        width={24}
+                        height={24}
+                      />
+
+                      <Input placeholder="Event location or Online" {...field} className="bg-grey-50 h-[54px] focus-visible:ring-offset-0 placeholder:text-grey-500 rounded-full p-regular-16 px-4 py-3 border-none focus-visible:ring-transparent" />
+                    </div>
+
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+                         <FormField
+          control={form.control}
+          name="eventDescription"
+          render={({ field }) => (
+            <FormItem className='w-full'>
+              <FormLabel>Event Type</FormLabel>
+              <FormControl>
+              <Select >
+      <SelectTrigger className="select-field">
+        <SelectValue placeholder="Select a Types of event" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Event-Type</SelectLabel>
+          <SelectItem value="online">Online</SelectItem>
+          <SelectItem value="Physical">Physical</SelectItem>
+
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+              </FormControl>
+              <FormDescription>
+                Select Type
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+  
         </div>
         <Button type="submit">Submit</Button>
       </form>
--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,8 @@ export type CreateUserParams = {
     email: string
     image: string
   }
+
+  export type EventCategoryType={
+    name:string
+    categoryDescription:string
+  }
