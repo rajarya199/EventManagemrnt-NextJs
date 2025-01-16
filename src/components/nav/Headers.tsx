d76f548c1245d628f2b9b80f393558f5fd0dc34f
rajarya199 <aryalrajan078@gmail.com>
2025-01-16 16:02:41 +0545

    change layout ,fix nav logo

--- src/components/nav/Headers.tsx
+++ src/components/nav/Headers.tsx
@@ -5,12 +5,13 @@ import { SignIn,SignedOut,SignInButton,UserButton ,SignedIn} from '@clerk/nextjs
 import NavItems from './Navitems'
 import NavHead from './NavHead'
 import MobileNav from './MobileNav'
+import Image from 'next/image'
 const Headers = () => {
   return (
     <header className='w-full border-b'>
             <div className="wrapper flex items-center justify-between">
         <Link href="/" className='w-36 font-poppins text-xl px-3'>
-        Event-Globe
+        <Image src="/assets/icons/logo.svg" alt='logo' width="48" height="28"/>
          </Link>
          <nav className='md:flex-between hidden w-full max-w-xs'>
           <NavHead/>
@@ -19,12 +20,15 @@ const Headers = () => {
        <NavItems/>
     </SignedIn>
         </nav>
-    <div className='flex w-2 justify-end gap-3'>
-      <SignedIn>
-        <UserButton/>
-        <MobileNav/>
-      </SignedIn>
+   
+    <div className='flex flex-row justify-end gap-3'>
+  <SignedIn>
+    <div className='flex flex-row gap-3'>
+      <UserButton />
+      <MobileNav />
     </div>
+  </SignedIn>
+</div>
          <SignedOut>
             <Button asChild className='rounded-md size="lg"  mx-1 '>
               <Link href="/sign-in">
