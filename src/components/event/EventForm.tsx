66da9e4edc7d0181a067b05f7233494f42709683
rajarya199 <aryalrajan078@gmail.com>
2025-01-27 16:34:23 +0545

    event form-ii

--- src/components/event/EventForm.tsx
+++ src/components/event/EventForm.tsx
@@ -274,7 +274,6 @@ const EventForm = ({userId,eventType}:EventFormProps) => {
                                   checked={field.value}
                                 id="isFree" className="mr-2 h-5 w-5 border-2 border-primary-500" />
                               </div>
-          
                             </FormControl>
                             <FormMessage />
                           </FormItem>
