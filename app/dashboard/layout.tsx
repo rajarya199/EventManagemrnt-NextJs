1d91a9e9b4d15f1a8b4e75f2227acb388119af86
rajarya199 <aryalrajan078@gmail.com>
2025-02-02 11:54:35 +0545

    dash layout

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,28 @@
+"use client"
+import React,{useState} from 'react'
+import Sidebar from "@/src/components/dash/Sidebar"
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+    const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+
+  return (
+    <div className="flex w-full min-h-screen">
+         <Sidebar
+        isCollapsed={isSidebarCollapsed}
+        setIsCollapsed={setIsSidebarCollapsed}
+      />
+      <main  className={`
+          flex-1 p-8 transition-all duration-300 ease-in-out
+          ${isSidebarCollapsed ? "ml-20" : "ml-64"}
+        `}>
+      {children}
+      </main>
+       
+    </div>
+  )
+}
