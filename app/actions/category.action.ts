c84795ffda77fbaaabe6dba16c4d16759979f07e
rajarya199 <aryalrajan078@gmail.com>
2025-02-07 16:49:19 +0545

    add category action

--- app/actions/category.action.ts
+++ app/actions/category.action.ts
@@ -1,9 +1,10 @@
 "use server"
+import { z } from "zod";
 import { revalidatePath } from 'next/cache'
 import { EventCategoryType } from '@/types'
 import db from '@/app/lib/db'
 import { handleError } from '../lib/utiils'
-
+import { categorySchema } from '@/src/lib/schema'
 export const saveCategory = async (data:EventCategoryType) => {
     try {
       const existingCategory = await db.category.findFirst({
@@ -40,4 +41,29 @@ export const saveCategory = async (data:EventCategoryType) => {
       console.error("Error:", error);
       return { success: false, message: "An unexpected error occurred" };
     }
+  }
+
+  export const addCategory=async(data:z.infer<typeof categorySchema>)=>{
+try{
+  const existingCategory = await db.category.findFirst({
+    where: { name: data.name },
+  });
+
+  if (existingCategory) {
+    return { success: false, message: "Category must be unique" };
+  }
+  const newCategory = await db.category.create({
+    data: {
+      name: data.name,
+      categoryDescription: data.categoryDescription,
+      imageUrl:data.imageUrl
+    },
+  });
+  return { success: true, data: newCategory };
+
+}
+catch(error){
+  handleError(error)
+  return { success: false, message:"Error saving category"};
+}
   }
