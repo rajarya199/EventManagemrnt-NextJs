7c9d49becd32488c46d89c0dcedd3d6bcecae3e3
rajarya199 <aryalrajan078@gmail.com>
2025-02-13 22:40:14 +0545

    catgory id param

--- app/dashboard/categories/update/[id]/page.tsx
+++ app/dashboard/categories/update/[id]/page.tsx
@@ -1,22 +1,20 @@
 import CategoryUpdateForm from '@/src/components/category/CategoryUpdateForm';
 import React from 'react';
 
-// Define the type for categoryProp
 interface CategoryProp {
   params: {
     id: string;
   };
 }
-
-// Use the interface as a type for props in UpdateCategory component
 const UpdateCategory: React.FC<CategoryProp> = ({ params }) => {
-  const { id } = params;
+  
 
   return (
     <div>
-      <CategoryUpdateForm categoryId={id} />
+      <CategoryUpdateForm categoryId={params.id} />
     </div>
   );
 };
 
 export default UpdateCategory;
+
