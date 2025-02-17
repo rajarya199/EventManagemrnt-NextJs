91b300b93f8e2e6f974809ba4a9443978979caac
rajarya199 <aryalrajan078@gmail.com>
2025-02-17 22:58:55 +0545

    event table

---
+++ src/components/event/EventTable.tsx
@@ -0,0 +1,102 @@
+"use client"
+import React,{useState,useEffect} from "react";
+import { MoreHorizontal, Search, Filter } from "lucide-react";
+import { FaEdit, FaTrash, FaPlus } from "react-icons/fa";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/src/components/ui/table"
+import { getAllEvents } from "@/app/actions/event.action";
+import { newDate } from "react-datepicker/dist/date_utils";
+ 
+const EventTable = () => {
+    const [events,setEvents]=useState <any[]>([])
+    useEffect(() => {
+        const fetchEvents = async () => {
+          try {
+            const response = await getAllEvents();
+            if (response.success && response.data) {
+              setEvents(response.data);
+            } else {
+              console.error("Failed to fetch events");
+            }
+          } catch (error) {
+            console.error("Failed to fetch events");
+          }
+        };
+        fetchEvents();
+      }, []);
+  return (
+    <div>
+      <div className="overflow-x-auto">
+      <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>Image</TableHead>
+              <TableHead>Event</TableHead>
+              <TableHead>Category</TableHead>
+              <TableHead>Type</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead>Start Time</TableHead>
+              <TableHead>End Time</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {events.map((event,index) => (
+              <TableRow key={event.id} className="hover:bg-gray-50">
+                <TableCell>{index+1}</TableCell>
+                <TableCell>
+                <div className="relative h-12 w-12">
+                                  <Image
+                                    src={event.imageUrl[0]}
+                                    alt={event.title}
+                                    className="object-cover rounded-sm"
+                                    fill
+                                  />
+                                </div>
+                </TableCell>
+               
+                <TableCell>{event.title}</TableCell>
+                <TableCell>{event.Category?.name}</TableCell>
+                <TableCell>{event.type}</TableCell>
+                <TableCell className="max-w-xs truncate">{event.eventDescription}</TableCell>
+                
+                <TableCell>{new Date(event.startTime).toLocaleDateString()}</TableCell>
+                <TableCell>{new Date(event.endTime).toLocaleDateString()}</TableCell>
+
+                <TableCell>{}</TableCell>
+
+                <TableCell className="text-right">
+                <div className="flex space-x-2">
+                    <Link href={`/dashboard/categories/update/${event.id}`}>
+                    <button
+                      className="text-blue-500 hover:text-green-700 text-2xl"
+                      aria-label="Edit"
+                    >
+                      <FaEdit />
+                    </button>
+                    </Link>
+                    <button className="text-gray-400 hover:text-gray-600">
+                    <MoreHorizontal className="w-5 h-5" />
+                  </button>
+                    </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  )
+}
+
+export default EventTable
