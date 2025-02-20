d351478d46cbbe12bce482155b1012265172ddf3
rajarya199 <aryalrajan078@gmail.com>
2025-02-20 16:47:56 +0545

    update user

--- app/actions/user.actions.ts
+++ app/actions/user.actions.ts
@@ -1,6 +1,6 @@
 "use server"
 import { revalidatePath } from 'next/cache'
-import { CreateUserParams } from '@/types'
+import { CreateUserParams,UpdateUserParams } from '@/types'
 import db from '@/app/lib/db'
 import { handleError } from '../lib/utiils'
 
@@ -16,6 +16,35 @@ export const createUser=async(user:CreateUserParams)=>{
 
     }
 }
+
+export async function updateUser(clerkId: string, userData: UpdateUserParams) {
+  console.log('hhhhhhhhhh')
+  try {
+    // Find the existing user by clerkId
+    const existingUser = await db.user.findUnique({
+      where: { clerkId },
+    });
+
+    if (!existingUser) {
+      console.error(`User with clerkId ${clerkId} not found`);
+      throw new Error('User not found');
+    }
+
+    // Update the user's information in the database
+    const updatedUser = await db.user.update({
+      where: { clerkId },
+      data: userData,
+    });
+
+    console.log("Updated user:", updatedUser);
+    
+    return updatedUser;
+  } catch (error) {
+    console.error('Error updating user:', error);
+    throw new Error('Failed to update user');
+  }
+}
+
 
 export async function getAllUsers(){
     try{
