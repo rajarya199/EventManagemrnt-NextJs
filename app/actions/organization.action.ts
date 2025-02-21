48f16daee281bd583032e1a3010e81feb741f9e6
rajarya199 <aryalrajan078@gmail.com>
2025-02-21 13:08:03 +0545

    save organization action

---
+++ app/actions/organization.action.ts
@@ -0,0 +1,35 @@
+"use server"
+import db from '@/app/lib/db'
+import { OrganizationFormValues } from '@/src/lib/schema';
+export async function addOrganization(orgData:OrganizationFormValues){
+    try{
+      const formattedContact = orgData.contact ? [orgData.contact] : [];
+  
+      const newOrganization = await db.organization.create({
+          data: {
+            name: orgData.name,
+            address:orgData.address,
+            contact: formattedContact,
+            description: orgData.description || null
+          },
+        });
+  
+         // Create Organizer Users
+      const organizerUsers = orgData.users.map(user => ({
+          organizationId: newOrganization.id,
+          userId: user.userId,
+          role: user.role,
+        }));
+  
+        await db.organizerUser.createMany({
+          data: organizerUsers,
+          skipDuplicates: true, // Prevent duplicates
+        });
+        return{success:true,data:newOrganization}
+    }
+    catch(error){
+      console.error('Unexpected error:', error)
+      return { success: false, message: 'An unexpected error occurred' }
+  
+    }
+  }
