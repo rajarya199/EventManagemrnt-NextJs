55f0a390bcb47f42158868a533480a04a6cbf80c
rajarya199 <aryalrajan078@gmail.com>
2025-02-26 00:04:02 +0545

    add org

---
+++ app/dashboard/organizations/add/page.tsx
@@ -0,0 +1,12 @@
+import OrganizationForm from '@/src/components/organization/OrganizationForm'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <OrganizationForm/>
+    </div>
+  )
+}
+
+export default page
