c34198d23b753335acfc432d2becdc63651ae9f0
rajarya199 <aryalrajan078@gmail.com>
2025-02-25 17:38:05 +0545

    user nav down

---
+++ src/components/profiledash/user-nav.tsx
@@ -0,0 +1,55 @@
+import { User2, Settings, Calendar, Phone, LogOut } from "lucide-react"
+import { Button } from "@/src/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/src/components/ui/dropdown-menu"
+import { Avatar, AvatarImage, AvatarFallback } from "@/src/components/ui/avatar"
+
+export function UserNav() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-10 w-10 rounded-full">
+          <Avatar>
+            <AvatarImage
+              src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-TU4TWL56BpEPtr3TLdBIMkrLqzR7xJ.png"
+              alt="John Snow"
+            />
+            <AvatarFallback>JS</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end">
+        <DropdownMenuLabel>WELCOME!</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <User2 className="mr-2 h-4 w-4" />
+          <span>My profile</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Settings className="mr-2 h-4 w-4" />
+          <span>Settings</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Calendar className="mr-2 h-4 w-4" />
+          <span>Activity</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Phone className="mr-2 h-4 w-4" />
+          <span>Support</span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Logout</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
