d8b3562c29a033e8270da1f079d470a766d9730f
rajarya199 <aryalrajan078@gmail.com>
2025-03-20 12:42:43 +0545

    past event page

---
+++ app/(main)/past-event/page.tsx
@@ -0,0 +1,16 @@
+import PastEventCard from '@/src/components/hero/PastEventCard'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-primary-50'>
+        <div className='wrapper'>
+        <h2 className="text-2xl font-semibold p-2">Past Events</h2>
+        <PastEventCard/>
+        </div>
+ 
+    </div>
+  )
+}
+
+export default page
