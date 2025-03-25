52beaf3d22b7fabef260cf083149cd0adf5fd8c6
rajarya199 <aryalrajan078@gmail.com>
2025-03-25 23:31:15 +0545

    validte ticket action

---
+++ app/actions/ticketValidation.action.ts
@@ -0,0 +1,59 @@
+"use server"
+import db from '@/app/lib/db'
+import { currentUser } from '@clerk/nextjs/server'
+
+export async function validateTicket(eventId:string,ticketId:string){
+    try{
+        const user = await currentUser()
+        if (!user) {
+            return { success: false, message: "Unauthorized: Please log in." };
+          }
+          const userId = user.publicMetadata?.userId as string
+
+        const ticket=await db.ticket.findUnique({
+            where:{id:ticketId},
+            include:{
+                TicketCategory:{
+                    include:{Event:true}
+                },
+            },
+        })
+        if(!ticket){
+            return{success:false,message:"Invalid ticket"}
+        }
+
+        const event = ticket.TicketCategory.Event;
+
+           // Ensure the logged-in user is the event organizer
+    if (event.organizerId !== userId) {
+        console.warn(`Access denied: User ${userId} is not the organizer of event ${event.id}`);
+
+        return { success: false, message: "Access denied: You are not the organizer of this event." };
+      }
+
+            // Ensure ticket belongs to the right event
+
+        if (event.id !== eventId) {
+            return { success: false, message: "Ticket does not match this event" };
+          }
+
+          if (ticket.status==="CheckedIn"){
+            return { success: false, message: "Ticket already used" };
+
+          }
+          
+          //update ticket status
+          await db.ticket.update({
+            where: { id: ticketId },
+            data: { status: "CheckedIn" },
+          });
+          return { success: true, data: ticket };
+     
+
+    }
+    catch(error){
+        console.error("Error validating ticket:", error);
+        return { success: false, message: "Error validating ticket" };
+
+    }
+}
