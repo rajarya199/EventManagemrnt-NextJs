855578c76482060283e17b57a634a1d8abc6403f
rajarya199 <aryalrajan078@gmail.com>
2025-04-02 22:44:41 +0545

    app-laucher  ii

--- src/components/profiledash/app-launcher.tsx
+++ src/components/profiledash/app-launcher.tsx
@@ -1,37 +1,48 @@
-import { Calendar, Mail, CreditCard, FileText, Map, ShoppingBag, Layers } from "lucide-react"
+import { Calendar, Mail, CreditCard, FileText, Map, ShoppingBag, Layers,Ticket ,Building2} from "lucide-react"
 import { Button } from "@/src/components/ui/button"
 import { Popover, PopoverContent, PopoverTrigger } from "@/src/components/ui/popover"
-
+import Link from "next/link"
 const apps = [
   {
     name: "Events",
     icon: Calendar,
     color: "bg-red-500",
+    href:"/profile/my-events"
   },
   {
-    name: "Email",
-    icon: Mail,
-    color: "bg-orange-500",
+    name: "Ticket",
+    icon: Ticket,
+    color: "bg-blue-500",
+      href:"/profile/my-tickets"
   },
   {
     name: "Payments",
     icon: CreditCard,
     color: "bg-blue-500",
+      href:"/profile"
   },
   {
     name: "Reports",
     icon: FileText,
     color: "bg-emerald-500",
+      href:"/profile"
   },
+  // {
+  //   name: "Maps",
+  //   icon: Map,
+  //   color: "bg-purple-500",
+  // },
   {
-    name: "Maps",
-    icon: Map,
+    name: "Organization",
+    icon: Building2,
     color: "bg-purple-500",
+      href:"/profile"
   },
   {
     name: "Booking",
     icon: ShoppingBag,
     color: "bg-yellow-500",
+      href:"/profile/my-bookings"
   },
 ]
 
@@ -47,8 +58,9 @@ export function AppLauncher() {
       <PopoverContent className="w-80" align="end">
         <div className="grid grid-cols-3 gap-4">
           {apps.map((app) => (
-            <Button
-              key={app.name}
+            <Link key={app.name} href={app.href} passHref>
+              <Button
+            
               variant="ghost"
               className="flex h-24 w-full flex-col items-center justify-center gap-2"
             >
@@ -57,6 +69,7 @@ export function AppLauncher() {
               </div>
               <span className="text-xs font-medium">{app.name}</span>
             </Button>
+            </Link>
           ))}
         </div>
       </PopoverContent>
