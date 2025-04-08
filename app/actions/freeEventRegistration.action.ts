e11baa5c6cec28f65778b553a7585045de3f22ac
rajarya199 <aryalrajan078@gmail.com>
2025-04-08 10:23:07 +0545

    save registration

---
+++ app/actions/freeEventRegistration.action.ts
@@ -0,0 +1,28 @@
+"use server"
+import db from '@/app/lib/db'
+
+export const saveRegistration=async({userId,eventId}:{userId:string,eventId:string})=>{
+    try{
+        const existingRegistration=await db.freeEventRegistration.findFirst({
+            where:{
+                userId,
+                eventId,
+                status:"CONFIRMED"
+            },
+        })
+        if(existingRegistration){
+            return{success:false,message:"You have already registered for this event"}
+        }
+        const newRegistration=await db.freeEventRegistration.create({
+            data:{
+                userId,
+                eventId,
+            },
+        })
+        return{success:true,data:newRegistration}
+    }
+    catch(error){
+        console.error("Error saving registration",error)
+        return{success:false,message:"An unexpected error occurred"}
+    }
+}
