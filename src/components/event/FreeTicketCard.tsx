4744245b856276b1830fdaf5213a6e1722dae6b5
rajarya199 <aryalrajan078@gmail.com>
2025-04-08 11:01:41 +0545

    available spot

--- src/components/event/FreeTicketCard.tsx
+++ src/components/event/FreeTicketCard.tsx
@@ -36,7 +36,8 @@ const FreeTicketCard = ({ event }: FreeTicketCardProps) => {
       });
     });
   };
-
+const takenPlace=event.FreeEventRegistrations.length
+const availablePlace=event.eventCapacity -takenPlace
   return (
     <div className="bg-white rounded-lg border border-gray-200 p-6 shadow-sm text-center">
       <h2 className="text-2xl font-bold text-gray-900 mb-4">Enjoy the Event 🎉</h2>
@@ -44,7 +45,7 @@ const FreeTicketCard = ({ event }: FreeTicketCardProps) => {
         <h3 className="text-lg font-medium text-gray-800">Register Your Place !!</h3>
         <p className="text-gray-600">This event is completely free!</p>
         <p className="mt-4 text-sm text-gray-700">
-          <strong>{event.eventCapacity}</strong> spots remaining
+          <strong>{availablePlace} /{event.eventCapacity}</strong> spots remaining
         </p>
       </div>
 
