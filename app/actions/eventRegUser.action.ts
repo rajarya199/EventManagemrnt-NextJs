dc92c886c62367ae140988a289953b3f09196a2a
rajarya199 <aryalrajan078@gmail.com>
2025-04-11 11:58:18 +0545

    get user reg event

--- app/actions/eventRegUser.action.ts
+++ app/actions/eventRegUser.action.ts
@@ -59,3 +59,31 @@ export async function getFreeEventRegUsers(eventId:string){
     return{success:false,message:"An unexpected error occured"}
  }
 }
+
+export async function getUserRegFreeEvent(userId:string){
+    try{
+        const registrations=await db.freeEventRegistration.findMany({
+            where:{userId},
+            include:{
+                User:{
+                    select:{id:true,
+                        fname:true,
+                        lname:true,
+                        email:true,
+                    },
+                },
+                Event:true,
+            },
+            orderBy:{createdAt:"desc"}
+        })
+        if(!registrations || registrations.length===0){
+            return{success:false,message:"No event has been registered by you"}
+        }
+        return{success:true,data:registrations}
+
+    }
+    catch(error){
+        console.error("error fetching the event registrations",error)
+        return{success:false,message:"An unexpected error occured"}
+    }
+}
