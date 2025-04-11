ac8f1142d093a205773334af5009949fde27e9d6
rajarya199 <aryalrajan078@gmail.com>
2025-04-11 11:57:45 +0545

    registerd evt page

---
+++ app/profile/my-registrations/page.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import { auth } from '@clerk/nextjs/server'
+import UserBookings from '@/src/components/bookings/UserBookings';
+import UserEventRegistration from '@/src/components/registration/UserEventRegistration';
+
+const page = async() => {
+    const { sessionClaims } = await auth();
+    const userId = sessionClaims?.id as string;
+  return (
+    <div className="wrapper bg-primary-50">
+      <UserEventRegistration userId={userId}/>
+    </div>
+  )
+}
+
+export default page
