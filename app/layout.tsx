3b9f67da56b11bd0348ff5952143346809250ee1
rajarya199 <aryalrajan078@gmail.com>
2025-04-22 10:04:56 +0545

    sonner in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
+import { Toaster, toast } from 'sonner'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+                  <Toaster position="bottom-right" richColors  />
+
         {children}
       </body>
     </html>
