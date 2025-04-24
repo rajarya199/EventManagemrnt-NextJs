f7365cfb86a6a6be3d705a4aff4375ea1c119ff2
rajarya199 <aryalrajan078@gmail.com>
2025-04-25 00:01:39 +0545

    dark/light theme

--- src/components/ticket/ManageTicketCard.tsx
+++ src/components/ticket/ManageTicketCard.tsx
@@ -45,19 +45,19 @@ ticket
   const availableTickets = ticket.totalStock - soldTickets;
   const status = availableTickets > 0 ? "Available" : "Sold Out";
   return (
-    <div className="flex items-center justify-between p-4 hover:bg-gray-50 border-b border-gray-100 last:border-b-0">
+    <div className="flex items-center justify-between p-4 roundex-xl hover:bg-gray-50 dark:hover:bg-gray-700 border-b border-gray-100 dark:border-gray-600 last:border-b-0">
       <div className="flex items-center space-x-3">
       <Ticket className="w-5 h-5 text-blue-500" />
         <div>
-          <h3 className="font-medium text-gray-900">{ticket.name}</h3>
-          <div className="flex items-center text-sm text-gray-500">
+          <h3 className="font-medium dark:text-gray-100 text-gray-900">{ticket.name}</h3>
+          <div className="flex items-center text-sm dark:text-gray-300 text-gray-500">
             <Users className="w-4 h-4 mr-1" />
             {soldTickets} / {ticket.totalStock} sold
           </div>
         </div>
       </div>
       <div className="text-right">
-        <div className="text-lg font-semibold text-gray-900">Rs.{ticket.ticketPrice}</div>
+        <div className="text-lg font-semibold dark:text-gray-100 text-gray-900">Rs.{ticket.ticketPrice}</div>
         <div
           className={`mt-1 px-1 py-0.5 text-xs font-medium rounded-full border inline-block ${getStatusStyle(status)}`}
         >
@@ -96,9 +96,9 @@ const ManageTicketCard = ({eventId}:TicketProps) => {
         );
       }
   return (
-    <div className="w-full max-w-2xl bg-white rounded-xl shadow-sm border border-gray-200">
-    <div className="p-6 border-b border-gray-200 flex justify-between items-center">
-      <h2 className="text-xl font-semibold text-gray-900">
+    <div className="w-full max-w-2xl bg-white dark:bg-primary-600 rounded-xl shadow-sm border border-gray-200">
+    <div className="p-6 border-b border-gray-200 dark:border-gray-500 flex justify-between items-center">
+      <h2 className="text-xl font-semibold text-gray-900 dark:text-gray-100">
         Tickets
       </h2>
       <Link
