ec2b74f8ea7899915029ceb9a2a59f00e5aa105e
rajarya199 <aryalrajan078@gmail.com>
2025-04-24 11:43:43 +0545

    d/l

--- app/(main)/events/add/page.tsx
+++ app/(main)/events/add/page.tsx
@@ -9,15 +9,18 @@ const AddEvent = async () => {
 
     return (
     <>
-       <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
+       <section className='bg-primary-50 dark:bg-primary-900 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
         <h3 className='wrapper  h3-bold text-center sm:text-left'>
         Create Your Event    
         </h3>
         </section>
-        <div className='wrapper my-8'>
+        <div className=' dark:bg-primary-900'>
+        <div className='wrapper py-8'>
             <EventForm userId={userId} eventType="Add"/>
 
         </div>
+        </div>
+        
     </>
  
   )
