8af4b353ba213b62226350271c2fb19fad743c17
rajarya199 <aryalrajan078@gmail.com>
2025-04-26 00:18:44 +0545

    theme

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 const DashBoard = () => {
   return (
-    <div>
+    <div className='dark:bg-primary-900'>
       <div className="mb-8">
-          <h1 className="text-2xl font-bold text-gray-800">Dashboard</h1>
-          <p className="text-gray-600">Welcome , Admin</p>
+          <h1 className="text-2xl font-bold dark:text-gray-100 text-gray-800">Dashboard</h1>
+          <p className="text-gray-600 dark:text-gray-300">Welcome , Admin</p>
           <DashboardStats/>
         </div>
      </div>
